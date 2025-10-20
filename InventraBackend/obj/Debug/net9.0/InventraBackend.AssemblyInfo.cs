36c2f801829f4b5e526203207dd81fabeef224ac
willowCox1221 <wcox@student.neumont.edu>
2025-10-20 16:25:47 -0600

    Made buttons for French, Spanish, and English

--- InventraBackend/obj/Debug/net9.0/InventraBackend.AssemblyInfo.cs
+++ InventraBackend/obj/Debug/net9.0/InventraBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventraBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a31ce3f8ed9db98aedfeed1431178928113315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5af03c20e0ba98363a622c3a6a2ad1e12bb9de8")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventraBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventraBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
